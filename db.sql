e119d9c62d0e8791ea3f7b9c72ed25488e382a5d
dienelopes <dienelopes7@gmail.com>
2022-11-30 09:54:31 -0300

    Adicionando novamente a lista de alunos, cursos, cadastro de usuarios e categorioas

---
+++ db.sql
@@ -0,0 +1,86 @@
+CREATE DATABASE db_escola;
+
+USE db_escola;
+
+CREATE TABLE tb_alunos (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    matricula VARCHAR(20) UNIQUE NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    status TINYINT NOT NULL,
+    genero VARCHAR(20) NOT NULL,
+    dataNascimento DATETIME NOT NULL,
+    cpf CHAR(11) UNIQUE NOT NULL
+);
+
+CREATE TABLE tb_professores (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    endereco VARCHAR(45) NOT NULL,
+    formacao VARCHAR(45) NOT NULL,
+    status TINYINT NOT NULL,
+    nome VARCHAR(100) NOT NULL,
+    cpf CHAR(11) UNIQUE NOT NULL
+);
+
+CREATE TABLE tb_user (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    profile VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE tb_categorias (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE tb_cursos (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50) NOT NULL,
+    cargaHoraria VARCHAR(50) NOT NULL,
+    descricao VARCHAR(100) UNIQUE NOT NULL,
+    status TINYINT NOT NULL,
+    categoria_id INT NOT NULL,
+    FOREIGN KEY (categoria_id) REFERENCES tb_categorias(id)
+);
+
+CREATE TABLE tb_user (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    profile VARCHAR(20) NOT NULL
+);
+
+INSERT INTO tb_user (name,email,password,profile)
+VALUES ('admin','admin@email.com','','admin');
+
+INSERT INTO tb_alunos 
+(nome, matricula, email, status, genero, dataNascimento, cpf)
+VALUES
+('Nenem', '1234123', 'nenem@email.com', true, 'Felino', '2001-09-12', '12312312312'),
+('Lobinho', '4434123', 'chiquim@email.com', true, 'Felino', '2000-12-31', '44455588812'),
+('Joaquim', '5534123', 'joaquim@email.com', true, 'Não informado', '1997-06-27', '09812312390');
+
+INSERT INTO tb_professores
+(nome, endereco, formacao, status, cpf)
+VALUES
+('Alessandro','Rua barca semi nova 123', 'HTML, CSS, JS, React', true, '12345612345'),
+('Allan','Rua idelfonso albano 222, ap 1403', 'SABE TUDO, BRABISSIMO', true, '99999999999'),
+('Gleidson', 'Rua oscar frança 88', 'Formado nas ruas', true, '22222222222');
+
+INSERT INTO tb_categorias (nome) 
+VALUES 
+('Profissionalizante'),
+('Tecnico'),
+('Graduação'),
+('Bacharelado'),
+('Pós-Graduação');
+
+INSERT INTO tb_cursos
+(nome, cargaHoraria, descricao, status, categoria_id)
+VALUES
+('FullStack','192','Vai ficar profissional',1,1);
+
+SELECT * FROM tb_alunos;
---
+++ src/Controller/AuthController.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Repository\UserRepository;
+use App\Security\UserSecurity;
+
+class AuthController extends AbstractController
+{
+    private UserRepository $userRepository;
+
+    public function __construct()
+    {
+        $this->userRepository = new UserRepository();
+    }
+
+    public function login(): void
+    {
+        if(false === empty($_POST))
+        {
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+
+            $user = $this->userRepository->findOneByEmail($email);
+
+            
+            if(false === $user)
+            {
+                die('email não existe');
+            }
+            
+            if(false === password_verify($password,$user->password))
+            {
+                die('senha incorreta');
+            }
+
+            UserSecurity::connect($user);
+
+            $this->redirect('/alunos/listar');
+
+        }
+        
+        // $this->render('auth/login', [], false);
+        $this->render('auth/login', navbar: false); // apenas a partir do PHP8
+        return;
+    }
+
+    public function logout(): void
+    {
+        UserSecurity::disconnect();
+
+        $this->redirect('/login');
+
+    }
+}
